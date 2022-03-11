c6f11f16993e10349c053d9085cd14894076122f
hyungyu Kim <ypsophila97@icloud.com>
2022-03-11 13:30:05 +0900

    [Chore] upload `About Me` project

---
+++ SampleAppsTutorials-AppleDeveloper/AboutMe/AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by kimhyungyu on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleAppsTutorials-AppleDeveloper/AboutMe/AboutMe/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by kimhyungyu on 2022/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+            
+            FunFactsView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
