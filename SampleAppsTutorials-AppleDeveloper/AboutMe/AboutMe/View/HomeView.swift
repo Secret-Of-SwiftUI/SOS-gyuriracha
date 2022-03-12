87ae01470e6da849e57085be9af3eedfbfc7e12d
hyungyu Kim <ypsophila97@icloud.com>
2022-03-12 18:02:07 +0900

    [Add] add HomeView

---
+++ SampleAppsTutorials-AppleDeveloper/AboutMe/AboutMe/View/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  AboutMe
+//
+//  Created by kimhyungyu on 2022/03/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Image(information.image)
+            // it allows the image to adapt to the available space on the screen.
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .padding(40)
+
+            Text(information.name)
+                .font(.title)
+        }
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
