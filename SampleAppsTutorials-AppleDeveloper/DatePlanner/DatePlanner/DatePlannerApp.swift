9ac474500903d2b053b4201abd6a4bed2db11623
hyungyu Kim <ypsophila97@icloud.com>
2022-03-13 12:46:11 +0900

    [Feat] configure DataPlannerApp with NavigationView

--- SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/DatePlannerApp.swift
+++ SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/DatePlannerApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct DatePlannerApp: App {
+    // Whenever the data changes, SwiftUI automatically updates all of the views that use (or observe) it.
+    @StateObject private var eventData = EventData()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                EventList()
+                Text("Select an Event")
+                    .foregroundStyle(.secondary)
+            }
+            .environmentObject(eventData)
         }
     }
 }
