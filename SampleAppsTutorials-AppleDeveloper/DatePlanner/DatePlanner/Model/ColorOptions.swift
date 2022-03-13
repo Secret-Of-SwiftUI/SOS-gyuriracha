0d8dddfef6642e3d77e478794396a91370da8fd2
hyun99999 <ypsophila97@icloud.com>
2022-03-13 16:15:41 +0900

    [Feat] implement random color option

--- SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/Model/ColorOptions.swift
+++ SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/Model/ColorOptions.swift
@@ -5,4 +5,30 @@
 //  Created by kimhyungyu on 2022/03/13.
 //
 
-import Foundation
+import SwiftUI
+
+struct ColorOptions {
+    static var all: [Color] = [
+        .primary,
+        .gray,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .mint,
+        .cyan,
+        .indigo,
+        .purple,
+    ]
+    
+    static var `default` : Color = Color.primary
+    
+    static func random() -> Color {
+        if let element = ColorOptions.all.randomElement() {
+            return element
+        } else {
+            return .primary
+        }
+        
+    }
+}
