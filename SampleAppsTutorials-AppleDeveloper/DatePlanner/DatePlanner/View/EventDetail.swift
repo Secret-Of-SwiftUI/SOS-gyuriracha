e4f724d04e7f203ee06fdcdea8935628f7faaee4
hyun99999 <ypsophila97@icloud.com>
2022-03-13 16:32:49 +0900

    [Feat] implement creatable or modifiable view

--- SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/View/EventDetail.swift
+++ SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/View/EventDetail.swift
@@ -7,14 +7,85 @@
 
 import SwiftUI
 
+/// creatable or modifiable view.
 struct EventDetail: View {
+    @Binding var event: Event
+    let isEditing: Bool
+    
+    @State private var isPickingSymbol = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+
+            HStack {
+                Button {
+                    isPickingSymbol.toggle()
+                } label: {
+                    Image(systemName: event.symbol)
+                        .sfSymbolStyling()
+                        .foregroundColor(event.color)
+                        .opacity(isEditing ? 0.3 : 1)
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal, 5)
+
+                if isEditing {
+                    TextField("New Event", text: $event.title)
+                        .font(.title2)
+                } else {
+                    Text(event.title)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+            }
+            
+            if isEditing {
+                DatePicker("Date", selection: $event.date)
+                    .labelsHidden()
+                    .listRowSeparator(.hidden)
+
+            } else {
+                HStack {
+                    Text(event.date, style: .date)
+                    Text(event.date, style: .time)
+                }
+                .listRowSeparator(.hidden)
+            }
+            
+            Text("Tasks")
+                .fontWeight(.bold)
+            
+            ForEach($event.tasks) { $item in
+                TaskRow(task: $item, isEditing: isEditing)
+            }
+            .onDelete(perform: { indexSet in
+                event.tasks.remove(atOffsets: indexSet)
+            })
+            
+            Button {
+                event.tasks.append(EventTask(text: "", isNew: true))
+            } label: {
+                HStack {
+                    Image(systemName: "plus")
+                    Text("Add Task")
+                }
+            }
+            .buttonStyle(.borderless)
+        }
+        #if os(iOS)
+        // use on iOS
+        .navigationBarTitleDisplayMode(.inline)
+        #endif
+        .sheet(isPresented: $isPickingSymbol) {
+            SymbolPicker(event: $event)
+        }
     }
 }
 
 struct EventDetail_Previews: PreviewProvider {
     static var previews: some View {
-        EventDetail()
+        Group {
+            EventDetail(event: .constant(Event.example), isEditing: true)
+        }
     }
 }
