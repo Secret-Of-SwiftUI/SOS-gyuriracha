f2fe75036dd6fda243065963fef80e7809f158d5
hyungyu Kim <ypsophila97@icloud.com>
2022-03-13 11:39:43 +0900

    [Chore] set project `DatePlanner`

---
+++ SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by kimhyungyu on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
