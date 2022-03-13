51f8363e5bc0da9bc34fad1132ed258622758b9a
hyun99999 <ypsophila97@icloud.com>
2022-03-13 16:52:15 +0900

    [Feat] implement Event Detail's row view

--- SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/View/TaskRow.swift
+++ SampleAppsTutorials-AppleDeveloper/DatePlanner/DatePlanner/View/TaskRow.swift
@@ -7,14 +7,48 @@
 
 import SwiftUI
 
+/// Event Detail's row view.
 struct TaskRow: View {
+    @Binding var task: EventTask
+    var isEditing: Bool
+    @FocusState private var isFocused: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button {
+                task.isCompleted.toggle()
+            } label: {
+                Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+            }
+            .buttonStyle(.plain)
+
+            if isEditing || task.isNew {
+                TextField("Task description", text: $task.text)
+                    .focused($isFocused)
+                    .onChange(of: isFocused) { newValue in
+                        if newValue == false {
+                            task.isNew = false
+                        }
+                    }
+
+            } else {
+                Text(task.text)
+            }
+
+            Spacer()
+        }
+        .padding(.vertical, 10)
+        .task {
+            if task.isNew {
+                isFocused = true
+            }
+        }
     }
+        
 }
 
 struct TaskRow_Previews: PreviewProvider {
     static var previews: some View {
-        TaskRow()
+        TaskRow(task: .constant(EventTask(text: "Do something!")), isEditing: false)
     }
 }
