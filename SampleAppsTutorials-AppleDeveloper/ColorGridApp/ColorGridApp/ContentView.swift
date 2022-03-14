42bfec4b97bfb5eb3a278ea1ef27d3612573beb8
hyun99999 <ypsophila97@icloud.com>
2022-03-14 21:31:55 +0900

    [Feat] implement LazyVGrid

--- SampleAppsTutorials-AppleDeveloper/ColorGridApp/ColorGridApp/ContentView.swift
+++ SampleAppsTutorials-AppleDeveloper/ColorGridApp/ColorGridApp/ContentView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    let columnLayout = Array(repeating: GridItem(), count: 3)
+
+     @State private var selectedColor = Color.gray
+
+     let allColors: [Color] = [
+         .pink,
+         .red,
+         .orange,
+         .yellow,
+         .green,
+         .mint,
+         .teal,
+         .cyan,
+         .blue,
+         .indigo,
+         .purple,
+         .brown,
+         .gray
+     ]
+
+     var body: some View {
+         VStack {
+             Text("Selected Color")
+                 .font(.body)
+                 .fontWeight(.semibold)
+                 .foregroundColor(selectedColor)
+                 .padding(10)
+             
+             ScrollView {
+                 LazyVGrid(columns: columnLayout) {
+                     ForEach(allColors, id: \.description) { color in
+                         Button {
+                             selectedColor = color
+                         } label: {
+                             RoundedRectangle(cornerRadius: 4.0)
+                                 .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                 .foregroundColor(color)
+                         }
+                         .buttonStyle(.plain)
+                     }
+                 }
+             }
+         }
+         .padding(10)
+     }
+ }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
