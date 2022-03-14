5d6c333abb51f3fe6064267bab013ec78379158e
hyun99999 <ypsophila97@icloud.com>
2022-03-14 20:30:34 +0900

    [Chore] upload `ColorGridApp` tutorial for Grid

---
+++ SampleAppsTutorials-AppleDeveloper/ColorGridApp/ColorGridApp/ColorGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGridAppApp.swift
+//  ColorGridApp
+//
+//  Created by kimhyungyu on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct ColorGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
