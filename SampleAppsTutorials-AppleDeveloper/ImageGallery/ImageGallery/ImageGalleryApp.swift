31420aa053cd56a7d5fbcf0a3d5b8a4f9a52504c
hyun99999 <ypsophila97@icloud.com>
2022-03-15 17:54:11 +0900

    [Chore] upload `ImageGallery` tutorial for grid of images
    how to create a grid of images, complete with photo-picking and -editing functionality.

---
+++ SampleAppsTutorials-AppleDeveloper/ImageGallery/ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by kimhyungyu on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
