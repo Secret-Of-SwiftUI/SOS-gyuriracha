bec46f474519562cbed38345bebe065e244a0fa0
hyun99999 <ypsophila97@icloud.com>
2022-03-19 21:57:52 +0900

    [Chore] upload `MemeCreator` tutorial for fetch from the internet
    fetch structured data from a server asynchronously.

---
+++ SampleAppsTutorials-AppleDeveloper/MemeCreator/MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by kimhyungyu on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
