1c58b6939f8750deba4660ec3c5a54b938af0c06
hyun99999 <ypsophila97@icloud.com>
2022-03-21 20:07:42 +0900

    [Feat] build meeting timer screen

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -11,17 +11,30 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
-        }
-        HStack {
-            VStack {
-                Text("Second Elapsed")
-                Label("300", systemImage: "hourglass.bottomhalf.fill")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
             }
-            VStack {
-                Text("Seconds Remaining")
-                Label("600", systemImage: "hourglass.tophalf.fill")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
             }
         }
+        .padding()
     }
 }
 
