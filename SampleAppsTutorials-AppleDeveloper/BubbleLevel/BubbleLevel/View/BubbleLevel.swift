41f7bc6550b6c013cfaabb9a22fa5783db93a73f
hyun99999 <ypsophila97@icloud.com>
2022-03-21 16:02:28 +0900

    [Add] add `BubbleLevel` use for task that diplay the tilit of device

---
+++ SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/View/BubbleLevel.swift
@@ -0,0 +1,20 @@
+//
+//  BubbleLevel.swift
+//  BubbleLevel
+//
+//  Created by kimhyungyu on 2022/03/21.
+//
+
+import SwiftUI
+
+struct BubbleLevel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BubbleLevel_Previews: PreviewProvider {
+    static var previews: some View {
+        BubbleLevel()
+    }
+}
