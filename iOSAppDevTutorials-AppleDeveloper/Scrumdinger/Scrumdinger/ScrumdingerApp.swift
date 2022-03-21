eca1587420a7cf8f34644c2858fd4257eb37036d
hyun99999 <ypsophila97@icloud.com>
2022-03-21 16:45:06 +0900

    [Chore] upload project `Scrumdinger` that helps users manage their daily scrums

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
