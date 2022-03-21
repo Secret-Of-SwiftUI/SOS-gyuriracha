4d3f669630ac7319ce6cb2a2e3b74f2e742739cd
hyun99999 <ypsophila97@icloud.com>
2022-03-21 16:01:11 +0900

    [Feat] implement a view that show roll and pitch

--- SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/View/OrientationDataView.swift
+++ SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/View/OrientationDataView.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 
+// roll is the degree of left and right tilt.
+// pitch is the degree of forward and backward tilt.
 struct OrientationDataView: View {
+    @EnvironmentObject var detector: MotionDetector
+
+    var rollString: String {
+        detector.roll.describeAsFixedLengthString()
+    }
+
+    var pitchString: String {
+        detector.pitch.describeAsFixedLengthString()
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Horizontal: " + rollString)
+                .font(.system(.body, design: .monospaced))
+            Text("Vertical: " + pitchString)
+                .font(.system(.body, design: .monospaced))
+        }
     }
 }
 
 struct OrientationDataView_Previews: PreviewProvider {
+    @StateObject static private var motionDetector = MotionDetector(updateInterval: 0.01).started()
+    
     static var previews: some View {
         OrientationDataView()
+            .environmentObject(motionDetector)
     }
 }
