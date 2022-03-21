4f273f003ee438221f200345ed61ce268050cc0c
hyun99999 <ypsophila97@icloud.com>
2022-03-21 15:01:18 +0900

    [Add] add `MotionDetector` to observe the motion of device

--- SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/MotionDetector.swift
+++ SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/MotionDetector.swift
@@ -5,16 +5,96 @@
 //  Created by kimhyungyu on 2022/03/21.
 //
 
-import SwiftUI
+import CoreMotion
+import UIKit
 
-struct MotionDetector: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class MotionDetector: ObservableObject {
+    private let motionManager = CMMotionManager()
+
+    private var timer = Timer()
+    private var updateInterval: TimeInterval
+
+    @Published var pitch: Double = 0
+    @Published var roll: Double = 0
+    @Published var zAcceleration: Double = 0
+
+    var onUpdate: (() -> Void) = {}
+    
+    private var currentOrientation: UIDeviceOrientation = .landscapeLeft
+    private var orientationObserver: NSObjectProtocol? = nil
+    let notification = UIDevice.orientationDidChangeNotification
+
+    init(updateInterval: TimeInterval) {
+        self.updateInterval = updateInterval
+    }
+    
+    func start() {
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        orientationObserver = NotificationCenter.default.addObserver(forName: notification, object: nil, queue: .main) { [weak self] _ in
+            switch UIDevice.current.orientation {
+            case .faceUp, .faceDown, .unknown:
+                break
+            default:
+                self?.currentOrientation = UIDevice.current.orientation
+            }
+        }
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates()
+            
+            timer = Timer.scheduledTimer(withTimeInterval: updateInterval, repeats: true) { _ in
+                self.updateMotionData()
+            }
+        } else {
+            print("Motion data isn't available on this device.")
+        }
+    }
+    
+    func updateMotionData() {
+        if let data = motionManager.deviceMotion {
+            (roll, pitch) = currentOrientation.adjustedRollAndPitch(data.attitude)
+            zAcceleration = data.userAcceleration.z
+            
+            onUpdate()
+        }
+    }
+
+    func stop() {
+        motionManager.stopDeviceMotionUpdates()
+        timer.invalidate()
+        if let orientationObserver = orientationObserver {
+            NotificationCenter.default.removeObserver(orientationObserver, name: notification, object: nil)
+        }
+        orientationObserver = nil
+    }
+
+    deinit {
+        stop()
     }
 }
 
-struct MotionDetector_Previews: PreviewProvider {
-    static var previews: some View {
-        MotionDetector()
+extension MotionDetector {
+    func started() -> MotionDetector {
+        start()
+        return self
+    }
+}
+
+extension UIDeviceOrientation {
+    func adjustedRollAndPitch(_ attitude: CMAttitude) -> (roll: Double, pitch: Double) {
+        switch self {
+        case .unknown, .faceUp, .faceDown:
+            return (attitude.roll, -attitude.pitch)
+        case .landscapeLeft:
+            return (attitude.pitch, -attitude.roll)
+        case .portrait:
+            return (attitude.roll, attitude.pitch)
+        case .portraitUpsideDown:
+            return (-attitude.roll, -attitude.pitch)
+        case .landscapeRight:
+            return (-attitude.pitch, attitude.roll)
+        @unknown default:
+            return (attitude.roll, attitude.pitch)
+        }
     }
 }
