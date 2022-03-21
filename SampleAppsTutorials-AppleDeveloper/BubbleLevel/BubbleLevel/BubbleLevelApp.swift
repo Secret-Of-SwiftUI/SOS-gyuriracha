53a9513b09bebb0adae442b672d2f481de784328
hyun99999 <ypsophila97@icloud.com>
2022-03-21 13:48:22 +0900

    [Chore] upload project `BubbleLevel` tutorial for device sensor
    access and display device sensor data by subscribing to notifications.

---
+++ SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/BubbleLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleLevelApp.swift
+//  BubbleLevel
+//
+//  Created by kimhyungyu on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct BubbleLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleAppsTutorials-AppleDeveloper/BubbleLevel/BubbleLevel/MotionDetector.swift
@@ -0,0 +1,20 @@
+//
+//  MotionDetector.swift
+//  BubbleLevel
+//
+//  Created by kimhyungyu on 2022/03/21.
+//
+
+import SwiftUI
+
+struct MotionDetector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MotionDetector_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionDetector()
+    }
+}
