0aa2e3205566f253f6fcba36d9c67797fe5e3154
hyun99999 <ypsophila97@icloud.com>
2022-03-22 21:36:38 +0900

    [Feat] add accessibility modifiers `CardView`

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/CardView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/CardView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct CardView: View {
     let scrum: DailyScrum
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                .labelStyle(.trailingIcon)            }
-            .padding()
-            .foregroundColor(scrum.theme.accentColor)
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
