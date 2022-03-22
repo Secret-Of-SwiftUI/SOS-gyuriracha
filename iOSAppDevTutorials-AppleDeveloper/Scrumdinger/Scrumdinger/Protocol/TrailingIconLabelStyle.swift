77bd08e7911f56899a54c0c7cd133ab51194d2a9
hyun99999 <ypsophila97@icloud.com>
2022-03-22 17:22:22 +0900

    [Add] add `LabelStyle` protocol that create consistent design

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Protocol/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
