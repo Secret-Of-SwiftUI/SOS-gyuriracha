0a6891a3e74f9fb8944668e1db8be39723ae19c7
hyun99999 <ypsophila97@icloud.com>
2022-03-22 14:00:53 +0900

    [Chore] create Color Theme

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -0,0 +1,37 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/22.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
