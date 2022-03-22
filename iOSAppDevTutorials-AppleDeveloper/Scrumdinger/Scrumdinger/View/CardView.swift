98481f75607741545aaeaba4d3a75c66d4ec38a9
hyun99999 <ypsophila97@icloud.com>
2022-03-22 17:07:56 +0900

    [Add] add `CardView` use for task that display a piece of data in the DailyScrum structure

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+                    .font(.caption)
+            }
+            .padding()
+            .foregroundColor(scrum.theme.accentColor)
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
