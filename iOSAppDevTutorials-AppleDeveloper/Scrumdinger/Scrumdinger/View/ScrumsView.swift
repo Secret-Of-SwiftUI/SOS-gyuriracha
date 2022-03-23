2bad0ec53dee16f86ff4deb8eba3cd272b29f1d4
hyun99999 <ypsophila97@icloud.com>
2022-03-23 10:52:31 +0900

    [Feat] implement summary view of the user's daily scrums with list

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
