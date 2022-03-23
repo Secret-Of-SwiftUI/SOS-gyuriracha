71f7a7cea2b13838fec43b57776386dc9f82d70d
hyun99999 <ypsophila97@icloud.com>
2022-03-23 11:02:22 +0900

    [Feat] DailyScrum conform to the Identifiable protocol for a more robust way to communicate identity

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ScrumsView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ScrumsView.swift
@@ -12,7 +12,7 @@ struct ScrumsView: View {
     
     var body: some View {
         List {
-            ForEach(scrums, id: \.title) { scrum in
+            ForEach(scrums) { scrum in
                 CardView(scrum: scrum)
                     .listRowBackground(scrum.theme.mainColor)
             }
