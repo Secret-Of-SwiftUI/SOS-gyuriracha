a9f624174ae55c8ad3792e8942cdc02d2356461c
hyun99999 <ypsophila97@icloud.com>
2022-03-23 16:24:46 +0900

    [Add] add a EditView for Scrum Details
    - add slider use for set length

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailEditView.swift
@@ -0,0 +1,34 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/03/23.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
+                        //The Text view won’t appear on screen, but VoiceOver uses it to identify the purpose of the slider.
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
