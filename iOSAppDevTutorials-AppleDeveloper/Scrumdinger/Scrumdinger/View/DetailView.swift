c877997b9c134cd89e38dfb31c88ff5fd2720648
hyun99999 <ypsophila97@icloud.com>
2022-03-23 12:26:13 +0900

    [Feat] implement flow that navigate between the list, detail, and timer screens

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -39,6 +41,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
