45f22f6ffd7dff3453bcb2b14b67c567132a04bb
hyun99999 <ypsophila97@icloud.com>
2022-03-23 12:02:24 +0900

    [Feat] implement visual components to DetailView

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
@@ -11,7 +11,22 @@ struct DetailView: View {
     let scrum: DailyScrum
 
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                }
+            }
+        }
     }
 }
 
