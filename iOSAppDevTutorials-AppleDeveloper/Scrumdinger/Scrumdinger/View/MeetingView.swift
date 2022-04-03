6f9488c522edf7640f996c51dc8ab649837adb6e
hyun99999 <ypsophila97@icloud.com>
2022-04-03 17:14:41 +0900

    [Feat] compose MeetingView and Header
    - add ScrumTimer for trackable

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
         var body: some View {
             ZStack {
                 RoundedRectangle(cornerRadius: 16.0)
                     .fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack {
