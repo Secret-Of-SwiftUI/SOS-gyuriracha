4b47a59195fe199727a84fa526ce5b290f7dcb73
hyun99999 <ypsophila97@icloud.com>
2022-04-05 16:38:27 +0900

    [Add] add `History` structures that update list of the history

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/05.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
