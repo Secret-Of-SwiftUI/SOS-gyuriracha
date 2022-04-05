2bece9a602f7712a81381633d3c509c1f3c47193
hyun99999 <ypsophila97@icloud.com>
2022-04-05 15:28:09 +0900

    [Add] add life cycle events for scrum timer class to reset, start, stop

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
-        var body: some View {
-            ZStack {
-                RoundedRectangle(cornerRadius: 16.0)
-                    .fill(scrum.theme.mainColor)
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
@@ -29,6 +29,13 @@ struct MeetingView: View {
             }
             .padding()
             .foregroundColor(scrum.theme.accentColor)
+            .onAppear {
+                scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+                scrumTimer.startScrum()
+            }
+            .onDisappear {
+                scrumTimer.stopScrum()
+            }
             .navigationBarTitleDisplayMode(.inline)
         }
     }
