3da7d3128fd074ffeea18cfd24702d6cbcbab169
hyun99999 <ypsophila97@icloud.com>
2022-04-06 18:18:29 +0900

    [Add] create `ScrumStore` that use for store app data

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,21 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/05.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                       in: .userDomainMask,
+                                       appropriateFor: nil,
+                                       create: false)
+            .appendingPathComponent("scrums.data")
+    }
+}
