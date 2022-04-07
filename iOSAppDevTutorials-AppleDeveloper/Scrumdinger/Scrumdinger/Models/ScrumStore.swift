290b24a43ceec0c09b6700e7f3b93ed32e3a4369
hyun99999 <ypsophila97@icloud.com>
2022-04-07 20:42:01 +0900

    [Feat] add method to load data

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/ScrumStore.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/ScrumStore.swift
@@ -18,4 +18,26 @@ class ScrumStore: ObservableObject {
                                        create: false)
             .appendingPathComponent("scrums.data")
     }
+    
+    static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(dailyScrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
 }
