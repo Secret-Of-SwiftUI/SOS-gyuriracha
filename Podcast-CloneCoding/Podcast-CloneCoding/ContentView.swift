c368aeb1a68fc3eca3115fc80bab8882be1faae6
hyun99999 <ypsophila97@icloud.com>
2022-04-10 23:34:58 +0900

    chore: Podcast clone coding project setting (#2)

---
+++ Podcast-CloneCoding/Podcast-CloneCoding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
