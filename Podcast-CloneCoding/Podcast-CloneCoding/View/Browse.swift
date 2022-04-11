ed3e914faa803411b15f671efbf322a2f82773c2
hyun99999 <ypsophila97@icloud.com>
2022-04-12 01:34:25 +0900

    add: create `Browse` view to do display lists

---
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Browse.swift
@@ -0,0 +1,20 @@
+//
+//  Browse.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/04/12.
+//
+
+import SwiftUI
+
+struct Browse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Browse_Previews: PreviewProvider {
+    static var previews: some View {
+        Browse()
+    }
+}
