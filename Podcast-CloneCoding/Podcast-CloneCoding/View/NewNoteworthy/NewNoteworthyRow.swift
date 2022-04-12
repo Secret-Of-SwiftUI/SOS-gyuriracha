0136a8d6f794133ced0c9fd12e79eb2914649424
hyun99999 <ypsophila97@icloud.com>
2022-04-12 18:07:20 +0900

    fix: fix file name to `NewNoteworthyRow`

--- Podcast-CloneCoding/Podcast-CloneCoding/View/NewNoteworthy/NewNoteworthyRow.swift
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/NewNoteworthy/NewNoteworthyRow.swift
@@ -1,5 +1,5 @@
 //
-//  NewNoteworthy.swift
+//  NewNoteworthyRow.swift
 //  Podcast-CloneCoding
 //
 //  Created by kimhyungyu on 2022/04/12.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct NewNoteworthy: View {
+struct NewNoteworthyRow: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct NewNoteworthy_Previews: PreviewProvider {
+struct NewNoteworthyRow_Previews: PreviewProvider {
     static var previews: some View {
-        NewNoteworthy()
+        NewNoteworthyRow()
     }
 }
