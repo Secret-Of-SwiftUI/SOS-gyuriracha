b98c14349fbc4e32e9995cfa20ff04bc70051292
hyun99999 <ypsophila97@icloud.com>
2022-04-12 16:08:38 +0900

    fix: list structure to lazyVGrid
    - it's hard to remove separator line

--- Podcast-CloneCoding/Podcast-CloneCoding/View/Featured/FeaturedRow.swift
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Featured/FeaturedRow.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct FeaturedRow: View {
+    
+    // MARK: - Properties
+    
+    struct FeaturedData: Hashable, Identifiable {
+        var id: Int
+        var featuredCategory: String
+        var featuredTitle: String
+        var featuredSubtitle: String
+    }
+    
+    var featuredData: [FeaturedData]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 10) {
+                ForEach(featuredData) { featured in
+                    VStack(alignment: .leading) {
+                        Divider()
+                        Text(featured.featuredCategory)
+                            .font(.caption)
+                            .foregroundColor(.podcastMainColor)
+                            .bold()
+                        Text(featured.featuredTitle)
+                            .font(.title2)
+                        Text(featured.featuredSubtitle)
+                            .font(.title2)
+                            .foregroundColor(.secondary)
+                        Image("featuredImage1")
+                            .resizable()
+                            .scaledToFill()
+                            .cornerRadius(5)
+                    }
+                }
+            }
+        }
+        .frame(height: 245)
     }
 }
 
 struct FeaturedRow_Previews: PreviewProvider {
     static var previews: some View {
-        FeaturedRow()
+        FeaturedRow(featuredData: [FeaturedRow.FeaturedData(id: 0, featuredCategory: "FEATURED PODCAST", featuredTitle: "댓글 읽어주는 기자들", featuredSubtitle: "안에서 보는 미디어"),
+                                   FeaturedRow.FeaturedData(id: 1, featuredCategory: "FEATURED", featuredTitle: "Speak English with ESLPod.com - ...", featuredSubtitle: "Listen in to speak English fast")])
     }
 }
--- Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Browse.swift
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Browse.swift
@@ -11,14 +11,19 @@ struct Browse: View {
     
     // MARK: - Properties
     
-    private var category = ["", "최신 및 추천 항목", "추천 채널", "인기 프로그램", "인기 에피소드", "새로운 발견", "많이 듣는 프로그램", "어린이 세상", "뉴스 따라잡기", "코미디", "Art", "카테고리 둘러보기", "팟캐스트 바로가기"]
+    private let coloumns: [GridItem] = [
+        GridItem(.flexible())
+    ]
+//    ["", "최신 및 추천 항목", "추천 채널", "인기 프로그램", "인기 에피소드", "새로운 발견", "많이 듣는 프로그램", "어린이 세상", "뉴스 따라잡기", "코미디", "Art", "카테고리 둘러보기", "팟캐스트 바로가기"]
     
     // MARK: - View
     
     var body: some View {
         NavigationView {
-            List {
-                FeaturedRow()
+            LazyVGrid(columns: coloumns) {
+                FeaturedRow(featuredData: [FeaturedRow.FeaturedData(id: 0, featuredCategory: "FEATURED PODCAST", featuredTitle: "댓글 읽어주는 기자들", featuredSubtitle: "안에서 보는 미디어"),
+                                           FeaturedRow.FeaturedData(id: 1, featuredCategory: "FEATURED", featuredTitle: "Speak English with ESLPod.com - ...", featuredSubtitle: "Listen in to speak English fast"),
+                                           FeaturedRow.FeaturedData(id: 2, featuredCategory: "FEATURED CHANNEL", featuredTitle: "신사임당의 돈 얘기 하는 곳", featuredSubtitle: "매일 더하는 경제 지식")])
                 FeaturedChannelsRow()
                 TopShowsRow()
                 TopEpisodesRow()
@@ -27,12 +32,14 @@ struct Browse: View {
                 KidsCornerRow()
                 CatchUpOnTheNewsRow()
                 ComedyRow()
-                ArtRow()
-//                BrowseByCategoryRow()
-//                PodcastsQuickLinksRow()
+                //                ArtRow()
+                
+                //                BrowseByCategoryRow()
+                //                PodcastsQuickLinksRow()
             }
             .listStyle(.inset)
             .navigationTitle("Browse")
+            
         }
     }
 }
