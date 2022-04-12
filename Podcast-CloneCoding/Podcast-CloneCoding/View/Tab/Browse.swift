7270e03ab313c07e57eeac7d92f8fb3634c19844
hyun99999 <ypsophila97@icloud.com>
2022-04-12 14:05:54 +0900

    feat: create the overall structure (#4)
    - create list structure
    - create tab view

--- Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Browse.swift
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Browse.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct Browse: View {
+    
+    // MARK: - Properties
+    
     private var category = ["", "최신 및 추천 항목", "추천 채널", "인기 프로그램", "인기 에피소드", "새로운 발견", "많이 듣는 프로그램", "어린이 세상", "뉴스 따라잡기", "코미디", "Art", "카테고리 둘러보기", "팟캐스트 바로가기"]
+    
+    // MARK: - View
     
     var body: some View {
         NavigationView {
@@ -31,6 +36,8 @@ struct Browse: View {
         }
     }
 }
+
+// MARK: - Previews
 
 struct Browse_Previews: PreviewProvider {
     static var previews: some View {
