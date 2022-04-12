49ee9628b6f24c4b9f0ce6e0c9705a9cd27eaeab
hyun99999 <ypsophila97@icloud.com>
2022-04-12 14:46:00 +0900

    add: make `Color+` and use for main color as type property

---
+++ Podcast-CloneCoding/Podcast-CloneCoding/Resources/Extensions/Color+.swift
@@ -0,0 +1,20 @@
+//
+//  Color+.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/04/12.
+//
+
+import SwiftUI
+
+struct Color_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color__Previews: PreviewProvider {
+    static var previews: some View {
+        Color_()
+    }
+}
