d6755f12c6ccd6d13e28ba33322d942343f7733e
hyun99999 <ypsophila97@icloud.com>
2022-04-12 19:28:56 +0900

    add: create model `FeaturedChannels`

---
+++ Podcast-CloneCoding/Podcast-CloneCoding/Model/FeaturedChannels.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedChannels.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/04/12.
+//
+
+import Foundation
+
+struct FeaturedChannels: Identifiable {
+    let id = UUID()
+    let thumbnail: String
+    let detail: String
+}
