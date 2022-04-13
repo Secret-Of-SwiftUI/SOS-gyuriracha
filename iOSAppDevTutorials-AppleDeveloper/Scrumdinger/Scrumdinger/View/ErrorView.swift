ddd7e17ed0e5b4a817293627dc407bd3b538d27c
hyun99999 <ypsophila97@icloud.com>
2022-04-13 18:52:55 +0900

    add: create an `ErrorView`
    - this view presents a description of the error with some guidance

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ErrorView.swift
@@ -0,0 +1,44 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/13.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
