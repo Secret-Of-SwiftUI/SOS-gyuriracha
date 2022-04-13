d3b4d909913244d922cb3fb4bd49f662434956d3
hyun99999 <ypsophila97@icloud.com>
2022-04-13 16:44:21 +0900

    feat: load data on App launch

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    @StateObject private var store = ScrumStore()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: $scrums)
+                ScrumsView(scrums: $store.scrums)
+            }
+            .onAppear {
+                ScrumStore.load { result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let scrums):
+                        store.scrums = scrums
+                    }
+                }
             }
         }
     }
