b026672e4e5b55f00497c5a3f8ef4637f24982ae
hyun99999 <ypsophila97@icloud.com>
2022-04-13 19:40:37 +0900

    add: create `MeetingTimerView`
    - this view presents progress and current speaker's name

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingTimerView.swift
@@ -0,0 +1,38 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/13.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                Text(currentSpeaker)
+                    .font(.title)
+            }
+            .accessibilityElement(children: .combine)
+            .foregroundStyle(theme.accentColor)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, theme: .yellow)
+    }
+}
--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
@@ -20,8 +20,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
