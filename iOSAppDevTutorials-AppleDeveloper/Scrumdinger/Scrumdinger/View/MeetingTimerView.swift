4b45b3b2fbd1c1e73b8a5611c14a20354ec7a431
hyun99999 <ypsophila97@icloud.com>
2022-04-13 20:02:12 +0900

    feat: add arc to circle

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingTimerView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingTimerView.swift
@@ -19,11 +19,24 @@ struct MeetingTimerView: View {
         Circle()
             .strokeBorder(lineWidth: 24)
             .overlay {
-                Text(currentSpeaker)
-                    .font(.title)
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
             }
-            .accessibilityElement(children: .combine)
-            .foregroundStyle(theme.accentColor)
+            .overlay  {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12)
+                    }
+                }
+            }
+            .padding(.horizontal)
     }
 }
 
