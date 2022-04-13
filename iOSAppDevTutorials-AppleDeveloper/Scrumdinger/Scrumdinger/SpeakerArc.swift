3730390785d35ffafac64e0e048662e497d289b1
hyun99999 <ypsophila97@icloud.com>
2022-04-13 19:52:46 +0900

    add: create `SpeakerArc`
    - this view conform Shape.
    - this view calculate the length of the portional speaker

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/SpeakerArc.swift
@@ -0,0 +1,32 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/13.
+//
+
+import SwiftUI
+
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+
+    private var degreesPerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
+    }
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeaker * Double(speakerIndex) + 1.0)
+    }
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
