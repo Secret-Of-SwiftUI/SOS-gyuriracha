4b31ea1e5e2337548aadacfd955a003025cc7b6f
hyun99999 <ypsophila97@icloud.com>
2022-04-13 17:17:23 +0900

    faet: save data in inactive state

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -14,7 +14,13 @@ struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: $store.scrums)
+                ScrumsView(scrums: $store.scrums) {
+                    ScrumStore.save(scrums: store.scrums) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
             }
             .onAppear {
                 ScrumStore.load { result in
--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ScrumsView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/ScrumsView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
     @State private var newScrumData = DailyScrum.Data()
+    let saveAction: ()->Void
     
     var body: some View {
         List {
@@ -40,17 +42,36 @@ struct ScrumsView: View {
             }
         }
         .sheet(isPresented: $isPresentingNewScrumView) {
-            NavigationView {
-                DetailEditView(data: $newScrumData)
-            }
-        }
+                    NavigationView {
+                        DetailEditView(data: $newScrumData)
+                            .toolbar {
+                                ToolbarItem(placement: .cancellationAction) {
+                                    Button("Dismiss") {
+                                        isPresentingNewScrumView = false
+                                        newScrumData = DailyScrum.Data()
+                                    }
+                                }
+                                ToolbarItem(placement: .confirmationAction) {
+                                    Button("Add") {
+                                        let newScrum = DailyScrum(data: newScrumData)
+                                        scrums.append(newScrum)
+                                        isPresentingNewScrumView = false
+                                        newScrumData = DailyScrum.Data()
+                                    }
+                                }
+                            }
+                    }
+                }
+                .onChange(of: scenePhase) { phase in
+                    if phase == .inactive { saveAction() }
+                }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
         }
     }
 }
