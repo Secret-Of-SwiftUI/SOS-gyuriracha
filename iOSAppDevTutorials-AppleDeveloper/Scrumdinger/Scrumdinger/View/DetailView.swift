23e961be560646340d3b518f0172a499c370a8c7
hyun99999 <ypsophila97@icloud.com>
2022-04-15 00:42:13 +0900

    feat: intergrate the history view into `Scrumdinger`

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/DetailView.swift
@@ -48,9 +48,11 @@ struct DetailView: View {
                     Label("No meetings yet", systemImage: "calendar.badge.exclamationmark")
                 }
                 ForEach(scrum.history) { history in
-                    HStack {
-                        Image(systemName: "calendar")
-                        Text(history.date, style: .date)
+                    NavigationLink(destination: HistoryView(history: history)) {
+                        HStack {
+                            Image(systemName: "calendar")
+                            Text(history.date, style: .date)
+                        }
                     }
                 }
             }
