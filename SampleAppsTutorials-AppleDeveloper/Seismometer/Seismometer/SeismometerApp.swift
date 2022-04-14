9dcd069c1f70ca8851a4d5a44e0ad67d389346ce
hyun99999 <ypsophila97@icloud.com>
2022-04-15 02:12:15 +0900

    chore: update `Seismometer` project

---
+++ SampleAppsTutorials-AppleDeveloper/Seismometer/Seismometer/SeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp.swift
+//  Seismometer
+//
+//  Created by kimhyungyu on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
