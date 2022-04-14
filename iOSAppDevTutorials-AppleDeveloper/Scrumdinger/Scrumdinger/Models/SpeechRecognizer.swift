928bcee8dfb3fb569c4d321ef0eb39ccf0d6423c
hyun99999 <ypsophila97@icloud.com>
2022-04-15 00:23:47 +0900

    add: create `SpeechRecognizer` task for request access

---
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/SpeechRecognizer.swift
@@ -0,0 +1,20 @@
+//
+//  SpeechRecognizer.swift
+//  Scrumdinger
+//
+//  Created by kimhyungyu on 2022/04/15.
+//
+
+import SwiftUI
+
+struct SpeechRecognizer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeechRecognizer_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechRecognizer()
+    }
+}
