7d26f0ba122fad88c82e5a84cf19b50464215f74
hyun99999 <ypsophila97@icloud.com>
2022-04-15 00:40:16 +0900

    add: create `HistoryView` task for display meeting text and attendees

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/History.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/Models/History.swift
@@ -12,11 +12,13 @@ struct History: Identifiable, Codable {
     let date: Date
     var attendees: [DailyScrum.Attendee]
     var lengthInMinutes: Int
-    
-    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+    var transcript: String?
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transcript: String? = nil) {
         self.id = id
         self.date = date
         self.attendees = attendees
         self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
     }
 }
--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
@@ -41,7 +41,7 @@ struct MeetingView: View {
             scrumTimer.stopScrum()
             speechRecognizer.stopTranscribing()
             isRecording = false
-            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60, transcript: speechRecognizer.transcript)
             scrum.history.insert(newHistory, at: 0)
         }
         .navigationBarTitleDisplayMode(.inline)
