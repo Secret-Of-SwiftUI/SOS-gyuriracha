3e5c64b1daa8f9bf7374f9b6a39ba29aedbf78a7
hyun99999 <ypsophila97@icloud.com>
2022-04-15 00:32:31 +0900

    feat: display recording indicators

--- iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
+++ iOSAppDevTutorials-AppleDeveloper/Scrumdinger/Scrumdinger/View/MeetingView.swift
@@ -21,7 +21,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, isRecording: isRecording, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
