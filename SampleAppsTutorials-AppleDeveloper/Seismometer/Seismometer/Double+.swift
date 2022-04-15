6d71afdd0b7f6cb84d41d73a04d003a722c19f7c
hyun99999 <ypsophila97@icloud.com>
2022-04-15 13:25:08 +0900

    use extension on Double to format text in an easy to read way

---
+++ SampleAppsTutorials-AppleDeveloper/Seismometer/Seismometer/Double+.swift
@@ -0,0 +1,20 @@
+//
+//  Double+.swift
+//  Seismometer
+//
+//  Created by kimhyungyu on 2022/04/15.
+//
+
+import Foundation
+
+extension Double {
+    func describeAsFixedLengthString(integerDigits: Int = 2, fractionDigits: Int = 2) -> String {
+        self.formatted(
+            .number
+                .sign(strategy: .always())
+                .precision(
+                    .integerAndFractionLength(integer: integerDigits, fraction: fractionDigits)
+                )
+        )
+    }
+}
