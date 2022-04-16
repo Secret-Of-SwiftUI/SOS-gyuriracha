536b2a93844752b9922f64c237630403fbff0083
hyun99999 <ypsophila97@icloud.com>
2022-04-16 16:37:42 +0900

    feat: add gesture with .gesture modifier

--- SampleAppsTutorials-AppleDeveloper/RecognizingGestures/RecognizingGestures/ContentView.swift
+++ SampleAppsTutorials-AppleDeveloper/RecognizingGestures/RecognizingGestures/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TapView: View {
+
+    @State private var color : Color = Color.primary
+    
+    var tapGesture: some Gesture {
+        TapGesture()
+            .onEnded {
+                withAnimation {
+                    color = Color.random()
+                }
+            }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Tap the rectangle to change its color")
+            Spacer()
+            Rectangle()
+                .foregroundColor(color)
+                .frame(width: 250, height: 125)
+                .gesture(tapGesture)
+            Spacer()
+        }
+        .navigationTitle("Tap")
+        .toolbar {
+            Button("Reset") {
+                color = Color.primary
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct TapView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TapView()
+    }
+}
+
+extension Color {
+    static func random() -> Color {
+        return Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
     }
 }
