cc9eef637011dae1d8c0fe93d27b2f6b713547e1
hyun99999 <ypsophila97@icloud.com>
2022-04-16 14:58:14 +0900

    chore: upload `RecognizingGesture` project

---
+++ SampleAppsTutorials-AppleDeveloper/RecognizingGestures/RecognizingGestures/RecognizingGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognizingGesturesApp.swift
+//  RecognizingGestures
+//
+//  Created by kimhyungyu on 2022/04/16.
+//
+
+import SwiftUI
+
+@main
+struct RecognizingGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
