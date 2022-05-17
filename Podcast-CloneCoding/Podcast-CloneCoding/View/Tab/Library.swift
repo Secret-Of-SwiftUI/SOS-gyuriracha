efb09027d9eba76893e0a7ec4f380155cd972c6b
hyun99999 <ypsophila97@icloud.com>
2022-05-17 16:40:08 +0900

    add: add view for each tab in TabView(#22)

---
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Library.swift
@@ -0,0 +1,20 @@
+//
+//  Library.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/05/17.
+//
+
+import SwiftUI
+
+struct Library: View {
+    var body: some View {
+        Text("This is Library View")
+    }
+}
+
+struct Library_Previews: PreviewProvider {
+    static var previews: some View {
+        Library()
+    }
+}
---
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/Tab/Search.swift
@@ -0,0 +1,20 @@
+//
+//  Search.swift
+//  Podcast-CloneCoding
+//
+//  Created by kimhyungyu on 2022/05/17.
+//
+
+import SwiftUI
+
+struct Search: View {
+    var body: some View {
+        Text("This is Search View")
+    }
+}
+
+struct Search_Previews: PreviewProvider {
+    static var previews: some View {
+        Search()
+    }
+}
