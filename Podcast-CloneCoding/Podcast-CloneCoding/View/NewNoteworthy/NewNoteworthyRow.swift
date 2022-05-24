67dd5c2dd23adc6ddb107d78dd54940d83b8a2c7
hyun99999 <ypsophila97@icloud.com>
2022-05-24 21:02:16 +0900

    feat: add context menu

--- Podcast-CloneCoding/Podcast-CloneCoding/View/NewNoteworthy/NewNoteworthyRow.swift
+++ Podcast-CloneCoding/Podcast-CloneCoding/View/NewNoteworthy/NewNoteworthyRow.swift
@@ -33,29 +33,28 @@ struct NewNoteworthyRow: View {
                 HStack(alignment: .top, spacing: 12) {
                     Spacer()
                     ForEach(newNoteworthys) { newNoteworthy in
-                        // TODO: - link to NewNoteworthyDetail view.
-//                        NavigationLink {
-//                            NewNoteworthyDetail()
-//                        } label: {
+                        NavigationLink {
+                            NewNoteworthyDetail(newNoteworthy: newNoteworthy)
+                        } label: {
                             NewNoteworthyItem(newNoteworthy: newNoteworthy)
-//                        }
-                            .contextMenu {
-                                Button(action: {}) {
-                                    Label("Follow Show", systemImage: "plus.circle")
-                                }
-                                Button(action: {}) {
-                                    Label("Go to Show", systemImage: "airplayaudio")
-                                }
-                                Button(action: {}) {
-                                    Label("Share Show", systemImage: "square.and.arrow.up")
-                                }
-                                Button(action: {}) {
-                                    Label("Copy Link", systemImage: "link")
-                                }
-                                Button(action: {}) {
-                                    Label("Report a Concern", systemImage: "exclamationmark.bubble")
-                                }
+                        }
+                        .contextMenu {
+                            Button(action: {}) {
+                                Label("Follow Show", systemImage: "plus.circle")
                             }
+                            Button(action: {}) {
+                                Label("Go to Show", systemImage: "airplayaudio")
+                            }
+                            Button(action: {}) {
+                                Label("Share Show", systemImage: "square.and.arrow.up")
+                            }
+                            Button(action: {}) {
+                                Label("Copy Link", systemImage: "link")
+                            }
+                            Button(action: {}) {
+                                Label("Report a Concern", systemImage: "exclamationmark.bubble")
+                            }
+                        }
                     }
                     Spacer()
                 }
