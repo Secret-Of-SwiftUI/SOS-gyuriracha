d6b5c6541b229b76e50d6839c23bad668370ae2f
hyun99999 <ypsophila97@icloud.com>
2022-07-20 07:37:42 +0900

    add: create PageContorl to custom UIPageControl (#27)
    UIViewControllerRepresentable is a wrapper for a UIKit view that you use to integrate that view into your SwiftUI view hierarchy.

---
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,25 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/07/20.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
