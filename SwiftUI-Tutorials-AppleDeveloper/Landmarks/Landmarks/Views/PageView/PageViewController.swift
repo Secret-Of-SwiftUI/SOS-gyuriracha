871b87f25711959a8259260e6d2c8fcfd965c4f0
hyun99999 <ypsophila97@icloud.com>
2022-07-20 06:48:41 +0900

    feat: define Coordinator to representable view's context (#27)

--- SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -11,16 +11,30 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-
+        
         return pageViewController
     }
-
+    
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator: NSObject {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
     }
 }
