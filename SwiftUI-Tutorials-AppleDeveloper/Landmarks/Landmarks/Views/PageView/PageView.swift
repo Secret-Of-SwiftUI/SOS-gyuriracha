92ace9ad889f0738ae34dd94b6fb8bc6ac11d5e3
hyun99999 <ypsophila97@icloud.com>
2022-07-20 07:32:08 +0900

    feat: conform UIPageViewControllerDelegate to update the binding (#27)

--- SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageView.swift
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
 
     var body: some View {
-        PageViewController(pages: pages)
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            Text("Current Page: \(currentPage)")
+        }
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
         PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
-                    .aspectRatio(3 / 2, contentMode: .fit)
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
--- SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -30,7 +30,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
 
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
 
@@ -63,6 +63,14 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
                 return controllers.first
             }
             return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
     }
 }
