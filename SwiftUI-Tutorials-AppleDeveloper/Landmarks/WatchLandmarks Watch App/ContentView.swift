594b018af364c553c285088f753ac6000a844c1a
hyun99999 <ypsophila97@icloud.com>
2022-10-24 22:06:09 +0900

    Chore: create WatchOS App Target (#29)

---
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by kimhyungyu on 2022/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
