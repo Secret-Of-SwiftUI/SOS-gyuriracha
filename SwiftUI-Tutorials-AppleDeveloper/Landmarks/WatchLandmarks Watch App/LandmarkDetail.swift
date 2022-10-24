b3d636d5cfb3fffc04d58f60b519326ed5a058a6
hyun99999 <ypsophila97@icloud.com>
2022-10-24 22:07:40 +0900

    Add: add LandmarkDetail view to display landmark (#29)

---
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/WatchLandmarks Watch App/LandmarkDetail.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Watch App
+//
+//  Created by kimhyungyu on 2022/10/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
+    var body: some View {
+        CircleImage(image: landmark.image.resizable())
+            .scaledToFill()
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        let modelData = ModelData()
+        return LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
+}
