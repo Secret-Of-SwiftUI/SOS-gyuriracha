5b3c5cf6f88e3f04e5735d11482f8096dcace640
hyun99999 <ypsophila97@icloud.com>
2022-10-25 19:55:05 +0900

    Feat: add conditional compilation (#31)
    - scene is needed for watchOS.

--- SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/LandmarksApp.swift
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/Landmarks/LandmarksApp.swift
@@ -12,11 +12,15 @@ import SwiftUI
 struct LandmarksApp: App {
     // Use the @StateObject attribute to initialize a model object for a given property only once during the life time of the app. 
     @StateObject private var modelData = ModelData()
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(modelData)
         }
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
