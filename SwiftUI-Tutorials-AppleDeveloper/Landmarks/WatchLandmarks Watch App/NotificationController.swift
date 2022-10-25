a71acdfc14a29cd29f6b23112c249a01ddc7b396
hyun99999 <ypsophila97@icloud.com>
2022-10-25 19:29:53 +0900

    Add: add NotificationController (#31)
    -  for using dynamic notification interface

---
+++ SwiftUI-Tutorials-AppleDeveloper/Landmarks/WatchLandmarks Watch App/NotificationController.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationController.swift
+//  WatchLandmarks Watch App
+//
+//  Created by kimhyungyu on 2022/10/25.
+//
+
+import Foundation
+import SwiftUI
+import WatchKit
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+    override var body: NotificationView {
+        NotificationView()
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
